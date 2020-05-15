1128d60f050eee77bff4dfe1a4eb56697039c70a
chenjie <rokiecoder@163.com>
2020-05-15 14:58:09 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,28 @@
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+    },
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: '[name].[contenthash].js'
+        filename: 'index.[contenthash].js'
+    },
+    plugins: [new HtmlWebpackPlugin({
+        title: 'XDML - 写代码啦',
+        template: 'src/assets/index.html'
+    })],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
     }
 };
+
